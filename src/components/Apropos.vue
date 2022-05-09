285db58a46adf773a263856f6a66ca939e4c12cc
Demeure Romain <demeureromain4@gmail.com>
2022-05-09 09:15:51 +0200

    Modif detail apropos.vue

--- src/components/Apropos.vue
+++ src/components/Apropos.vue
@@ -12,9 +12,9 @@
                 <div class="text">
                     
                     <div class="aboutDivText">
-                        <p class="aboutText">2015 - Je suis rentrée dans la programmation grace au jeu vidéo minecraft ou j'ai écrit mes premiers script en Java pour créer des mini-jeux et jouer avec mes amis.</p>
-                        <p class="aboutText">2018 - Je découvre une faille sur un jeu McDonald's que j'exploite avec du JavaScript ce qui me permet de manger gratuitement et légalement. Je découvre donc réellement la puissance de la programmation.</p>
-                        <p class="aboutText">2020 - Je décide de me lancer réellement dans la programmation, d'investir dans des formations et de mettre en oeuvre mes idées. Je commence à avoir de plus en plus de compétences, j'aimerais donc en faire mon métier.</p>
+                        <p class="aboutText"><span class="color">2015</span>  - Je suis rentrée dans la programmation grace au jeu vidéo Minecraft ou j'ai écrit mes premiers script en Java pour créer des mini-jeux et jouer avec mes amis.</p>
+                        <p class="aboutText"><span class="color">2017</span> - Je découvre une faille sur un jeu McDonald's que j'exploite avec du JavaScript ce qui me permet de manger gratuitement et légalement. Je découvre donc réellement la puissance de la programmation.</p>
+                        <p class="aboutText"><span class="color">2021</span> - Je décide de me lancer réellement dans la programmation, d'investir dans des formations et de mettre en oeuvre mes idées. Je commence à avoir de plus en plus de compétences, j'aimerais donc en faire mon métier.</p>
                     </div>
                     
                     <div class="aboutPicture">
@@ -63,6 +63,9 @@
     width: auto;
 }
 
+.color {
+    color: #64FFDA;
+}
 @media only screen and (min-width: 900px) {
 
     .container {
@@ -92,9 +95,9 @@
         margin: 20px 0;
     }
     .container .about .section .aboutPicture .me {
-        margin-top: 0;
         border-radius: 5px;
-        height: 250px;
+        height: 350px;
+        margin-top: 0;
         width: auto;
     }
 
